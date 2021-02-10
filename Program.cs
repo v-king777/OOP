72cbb08cf5956edcac32ef5b2aa6eece35c318c5
v-king777 <v.kolpashchikov@gmail.com>
2021-02-10 22:27:39 +0300

    modified:   Program.cs

--- Program.cs
+++ Program.cs
@@ -4,24 +4,80 @@ class Program
 {
     static void Main(string[] args)
     {
-        DerivedClass derivedClass = new DerivedClass();
-        derivedClass.Display();
+        var array = new Book[] {
+            new Book { Name = "Мастер и Маргарита", Author = "М.А. Булгаков" },
+            new Book { Name = "Отцы и дети", Author = "И.С. Тургенев" },
+        };
+        BookCollection collection = new BookCollection(array);
+
+        Console.ReadKey();
+
+        Book book = collection[1];
+        book = collection[-1];
+        book = collection[4];
+
+        Console.ReadKey();
+
+        book = collection["Мастер и Маргарита"];
+
+        Console.ReadKey();
     }
 }
 
-class BaseClass
+class Book
 {
-    public virtual void Display()
-    {
-        Console.WriteLine("Метод класса BaseClass");
-    }
+    public string Name;
+    public string Author;
 }
 
-class DerivedClass : BaseClass
+class BookCollection
 {
-    public override void Display()
+    private Book[] collection;
+
+    public BookCollection(Book[] collection)
     {
-        base.Display();
-        Console.WriteLine("Метод класса DerivedClass");
+        this.collection = collection;
+    }
+
+    // Индексатор по массиву
+    public Book this[int index]
+    {
+        get
+        {
+            // Проверяем, чтобы индекс был в диапазоне для массива
+            if (index >= 0 && index < collection.Length)
+            {
+                return collection[index];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private set
+        {
+            // Проверяем, чтобы индекс был в диапазоне для массива
+            if (index >= 0 && index < collection.Length)
+            {
+                collection[index] = value;
+            }
+        }
+    }
+
+    public Book this[string name]
+    {
+        get
+        {
+            for (int i = 0; i < collection.Length; i++)
+            {
+                if (collection[i].Name == name)
+                {
+                    return collection[i];
+                }
+            }
+
+            return null;
+        }
     }
 }
