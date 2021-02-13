4fdfc3b97d2ec67cc1fd8b7279a666ad681b0c9c
v-king777 <v.kolpashchikov@gmail.com>
2021-02-13 11:04:42 +0300

    new file:   Obj.cs

---
+++ Obj.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Obj
+    {
+        public void Display<T>(T param)
+        {
+            Console.WriteLine(param.ToString());
+        }
+    }
+}
